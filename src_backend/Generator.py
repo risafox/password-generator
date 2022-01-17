37ed76e16ef36d497e2b6fa056a99b1544b3ea30
vanessachow <vanessa.chow@hotmail.com>
2022-01-16 19:53:04 -0800

    implemented changes discussed during jan 9 meeting

--- src_backend/Generator.py
+++ src_backend/Generator.py
@@ -12,12 +12,12 @@ NO_AMBIGUOUS_SYMBOLS = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "
 NUMBERS              = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 
-def password_generator_main(answer_one, answer_two, answer_three, password_length, have_symbols, have_numbers,
-                            have_lowercase, have_uppercase, have_ambiguous_characters):
+def password_generator_main(answer_one, answer_two, answer_three, password_length, is_exact, have_symbols, have_numbers,
+                            have_uppercase, have_ambiguous_characters):
 
     # STEP 1: check if inputs are valid, and fail if any are invalid
     is_input_valid = input_validation(answer_one, answer_two, answer_three, password_length, have_symbols,
-                                      have_numbers, have_lowercase, have_uppercase, have_ambiguous_characters)
+                                      have_numbers, have_uppercase, have_ambiguous_characters)
 
     if is_input_valid != 0:
 
@@ -29,7 +29,7 @@ def password_generator_main(answer_one, answer_two, answer_three, password_lengt
                   " increase your desired password length to at least" +
                   f" {min(answer_one, answer_two, answer_three)}.")
         elif is_input_valid == 3:
-            print("ERROR: You must select at least one of 'include lowercase' or 'include uppercase'.")
+            print("ERROR: You must select 'include uppercase'.")
         elif is_input_valid == 4:
             print("ERROR: You must either deselect 'exclude ambiguous characters' or select" +
                   " 'include symbols'")
@@ -37,10 +37,11 @@ def password_generator_main(answer_one, answer_two, answer_three, password_lengt
         return ""
 
     # STEP 2: format answer strings into what the user specified
-    formatted_answers = answer_formatter(answer_one, answer_two, answer_three, have_lowercase, have_uppercase)
+    formatted_answers = answer_formatter(answer_one, answer_two, answer_three, have_uppercase)
 
     # STEP 3: check how much "space" was allotted for the password, and see which answers can fit within the
     #         specified password length
+
     answers_to_use, remaining_space = choose_answers_for_password(formatted_answers, password_length)
 
     # STEP 4: fill the remaining password space with numbers and symbols if the user desired
@@ -55,13 +56,13 @@ def password_generator_main(answer_one, answer_two, answer_three, password_lengt
 
 # Checks if any inputs are invalid, leading to an error and no password generated
 def input_validation(answer_one, answer_two, answer_three, password_length, have_symbols, have_numbers,
-                     have_lowercase, have_uppercase, have_ambiguous_characters):
+                     have_uppercase, have_ambiguous_characters):
 
     if password_length < 1:
         error_code = 1
     elif password_length < min(len(answer_one), len(answer_two), len(answer_three)):
         error_code = 2
-    elif have_lowercase == False and have_uppercase == False:
+    elif have_uppercase == False:
         error_code = 3
     elif have_symbols == False and have_ambiguous_characters == True:
         error_code = 4
@@ -72,19 +73,14 @@ def input_validation(answer_one, answer_two, answer_three, password_length, have
 
 
 # Format answer strings (user-specified case, strip whitespace, sort by increasing length)
-def answer_formatter(answer_one, answer_two, answer_three, have_lowercase, have_uppercase):
+def answer_formatter(answer_one, answer_two, answer_three, have_uppercase):
     not_formatted_answers = [answer_one, answer_two, answer_three]
     formatted_answers = []
 
     not_formatted_answers.sort(key=len)
 
     for answer in not_formatted_answers:
-        if have_lowercase and have_uppercase:
-            formatted_answer = answer.title()
-        elif have_lowercase:
-            formatted_answer = answer.lower()
-        else:
-            formatted_answer = answer.upper()
+        formatted_answer = answer.upper()
 
         formatted_answer = formatted_answer.replace(" ", "")
         formatted_answers.append(formatted_answer)
@@ -95,6 +91,7 @@ def answer_formatter(answer_one, answer_two, answer_three, have_lowercase, have_
 # Determine answers to be included in password and subsequent remaining space
 def choose_answers_for_password(answer_list, password_length):
     answers_to_use = []
+
     remaining_space = password_length
 
     for i in range(len(answer_list)):
@@ -111,6 +108,9 @@ def add_numbers_and_symbols(answers_to_add, num_characters_to_add):
 
     random.shuffle(answers_to_add)
 
+    if is_exact == False:
+            num_characters_to_add += random.randint(1, 5)
+
     if num_characters_to_add == 0:
         password = "".join(answers_to_add)
     elif num_characters_to_add != 0 and have_symbols == False and have_numbers == False:
@@ -124,6 +124,7 @@ def add_numbers_and_symbols(answers_to_add, num_characters_to_add):
 
         if have_ambiguous_characters:
             character_pool += ALL_SYMBOLS
+
         elif have_symbols:
             character_pool += NO_AMBIGUOUS_SYMBOLS
 
@@ -146,15 +147,15 @@ if __name__ == "__main__":
     answer_two = "Red Sox"
     answer_three = "car"
     password_length = 12
+    is_exact = True # when set to False, password_length is the minimum
     have_symbols = True
     have_numbers = True
-    have_lowercase = False
     have_uppercase = True
     have_ambiguous_characters = False
 
     for i in range(10):
-        generated_password = password_generator_main(answer_one, answer_two, answer_three, password_length,
-                                                     have_symbols, have_numbers, have_lowercase, have_uppercase,
+        generated_password = password_generator_main(answer_one, answer_two, answer_three, password_length, is_exact,
+                                                     have_symbols, have_numbers, have_uppercase,
                                                      have_ambiguous_characters)
         print(f"Your password on iteration {i} is: {generated_password}")
 
