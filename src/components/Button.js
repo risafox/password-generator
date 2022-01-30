8eb203ac8477280d1e22f1f6d67e6621d252c278
duffy <dufei353194284@gmail.com>
2022-01-29 21:16:23 -0500

    finished the skeleton of obtaining passwords from a pseudo hard coded backend, and displaying the obtained data on the frontend page

--- src/components/Button.js
+++
@@ -1,8 +0,0 @@
-const Button = ({text}) => {
-    return (
-        <div>
-        <button className="btn">{text}</button>
-        </div>
-    )
-}
-export default Button
--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-    return (
-        <header>
-            <h1>Password Generator</h1>
-        </header>
-    )
-}
-
-export default Header
